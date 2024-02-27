ac90d3666b015dc20230cbc6836d6bb9287f8db0
Sthabiso10 <70531177+Sthabiso10@users.noreply.github.com>
2024-02-27 15:51:41 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -24,7 +26,10 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme,
-      home: const CategoriesScreen(),
+      home: MealsScreen(
+        title: 'Some Category',
+        meals: dummyMeals,
+      ),
     );
   }
 }
